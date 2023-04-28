5de11c022fe7c7f3931006ead416e5a0fe7824a0
Eugene Clark <clarkjr2016@gmail.com>
2023-04-27 20:38:34 -0400

    I don't know what's going on

--- script.js
+++ script.js
@@ -1,50 +1,40 @@
-const gameBoard = (function () {
-  //Private variables
-  let gameBoardDisplay = ["x", "x", "x", "o", "o", "o", "x", "x", "x"];
+(function GameBoard() {
+  // The Gameboard represents the state of the board
+  const row = 3;
+  const column = 3;
+  const board = [];
 
-  return { gameBoardDisplay };
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+    for (let j = 0; j < column; j++) {
+      board[i].push(0);
+    }
+  }
+
+  const getBoard = () => board;
+
+  return { getBoard };
 })();
 
-// module pattern for gameboard and gameboard display
+function Players() {
+  const players = [
+    {
+      name: "Player One",
+      marker: "X",
+    },
 
-let player = function (marker) {
-  const isSelected = false;
+    {
+      name: "Player Two",
+      marker: "O",
+    },
+  ];
+  return { players };
+}
 
-  return { marker, isSelected };
-};
+(function displayController() {
+  function playerMove() {
+    console.log();
+  }
 
-// player factory function
-
-const displayController = (function () {
-  const top_left = document.querySelector(".game-squares.top-left");
-  top_left.innerText = gameBoard.gameBoardDisplay[0];
-
-  const top_middle = document.querySelector(".game-squares.top-middle");
-  top_middle.innerText = gameBoard.gameBoardDisplay[1];
-
-  const top_right = document.querySelector(".game-squares.top-right");
-  top_right.innerText = gameBoard.gameBoardDisplay[2];
-
-  const middle_left = document.querySelector(".game-squares.middle-left");
-  middle_left.innerText = gameBoard.gameBoardDisplay[3];
-
-  const middle_middle = document.querySelector(".game-squares.middle-middle");
-  middle_middle.innerText = gameBoard.gameBoardDisplay[4];
-
-  const middle_right = document.querySelector(".game-squares.middle-right");
-  middle_right.innerText = gameBoard.gameBoardDisplay[5];
-
-  const bottom_left = document.querySelector(".game-squares.bottom-left");
-  bottom_left.innerText = gameBoard.gameBoardDisplay[6];
-
-  const bottom_middle = document.querySelector(".game-squares.bottom-middle");
-  bottom_middle.innerText = gameBoard.gameBoardDisplay[7];
-
-  const bottom_right = document.querySelector(".game-squares.bottom-right");
-  bottom_right.innerText = gameBoard.gameBoardDisplay[8];
-
-  const x_button = document.querySelector("X");
-
-  const o_button = document.querySelector("O");
+  playerMove();
 })();
-// Module design for controlling how the gameboard display looks and how it will be returned
