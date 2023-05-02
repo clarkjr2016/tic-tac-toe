73965019b175ec63cf03593f4cb97f5d71d61522
Eugene Clark <clarkjr2016@gmail.com>
2023-05-02 06:29:57 -0400

    Getting a little lost in the logic of checking if a player won or not, but I'm getting closer!

--- script.js
+++ script.js
@@ -40,14 +40,6 @@ const GameFlow = (function () {
     const board = GameBoard.getBoard(); // getting the state of  the board in a variable so that I'm refrencing the original object and not just creating a copy
     // this is calling the the getBoard function prior to the knnowledge be executed on it
 
-    const endOfRound = function () {
-      board;
-      const markerCollectionArray = [];
-      for (let i = 0; i < board[0].length; i++) {
-        markerCollectionArray.push(board[0][i]);
-      }
-    };
-
     if (board[row][column] === 0) {
       board[row][column] = player.marker;
       console.log(board); // if the spot that the player chooses to place its marker is empty update it to that players marker
@@ -57,6 +49,10 @@ const GameFlow = (function () {
       console.log(board);
       // if it isn't just print a log saying that the spot is taken
     }
+  };
+
+  const endOfRound = function (board) {
+    winningCombos();
   };
 
   const refresh = function (board) {
@@ -80,10 +76,21 @@ const GameFlow = (function () {
       regularBoard[2],
     ];
 
-    const combos = [[regularBoard[0][0], regularBoard[0][1]]];
+    const combos = [
+      [regularBoardRows[0][0], regularBoardRows[0][1], regularBoardRows[0][2]],
+      [regularBoardRows[1][0], regularBoardRows[1][1], regularBoardRows[1][2]],
+      [regularBoardRows[2][0], regularBoardRows[2][1], regularBoardRows[2][2]],
+      /* --- this groups all the winning row combinations --- */
+      [regularBoardRows[0][0], regularBoardRows[1][0], regularBoardRows[2][0]],
+      [regularBoardRows[0][1], regularBoardRows[1][1], regularBoardRows[2][1]],
+      [regularBoardRows[0][2], regularBoardRows[1][2], regularBoardRows[2][2]],
+      /* --- this groups all of the winning column combinations --- */
+      [regularBoardRows[0][0], regularBoardRows[1][1], regularBoardRows[2][2]],
+      [regularBoardRows[0][2], regularBoardRows[1][1], regularBoardRows[2][0]],
+    ];
 
-    return regularBoardRows;
-  };
+    return combos;
+  }; // setting up the winning combos to be reviewed whenever a move is completed.
 
-  return { makeMove, refresh, winningCombos };
+  return { makeMove, refresh, endOfRound };
 })();
