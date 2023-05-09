79756392f0e869ac3d02f51a643c860c359b9f95
Eugene Clark <clarkjr2016@gmail.com>
2023-05-08 21:46:26 -0400

    Added the functionality to put player's markers on the board but I need to figure out how to make that spot only exist

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameBoard = (function () {
   //Private variables
   let gameBoardDisplay = Array.from(
-    document.querySelector(".gameboard").children
+    document.querySelector(".gameboard").children // creating an array consisting of the DOM elements within the gameboard div
   );
 
   gameBoardDisplay.values;
@@ -23,16 +23,26 @@ const Player = function (player, marker) {
 
 const GameFlow = (function () {
   const playerOne = Player("Player 1", "O");
-  const playerTwo = Player("Player 2", "X");
+  const playerTwo = Player("Player 2", "X"); // creating the players in the game
 
-  playerOne.isActive = true;
+  playerOne.isActive = true; //setting playerOne's active status to true so that it can be the default player at the begginning of the game
 
-  let playerArray = [playerOne, playerTwo];
+  let playerArray = [playerOne, playerTwo]; // placing the two players into an array to cycle between in a below function
 
   console.log(playerArray);
+  console.log(gameBoard.gameBoardDisplay);
 
   gameBoard.gameBoardDisplay.forEach((cell) => {
-    cell.removeEventListener;
+    cell.addEventListener("click", (e) => {
+      playerArray.forEach((player) => {
+        if (player.isActive === true) {
+          e.target.innerText = player.marker; // setting the cell to the player with the active status
+          player.isActive = false; // setting the player's marker that was originally true to false
+        } else {
+          player.isActive = true; // setting the other player's status to true so that it would be selected on the next click
+        }
+      });
+    }); // event listener to add marker to cells;
   });
 
   const x_button = document.querySelector("X");
