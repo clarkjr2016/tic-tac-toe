be2299b6955cbf72939e4a7c0be6629879f8f0f5
Eugene Clark <clarkjr2016@gmail.com>
2023-05-12 20:54:59 -0400

    By the grace of the Lord I figured out how to prevent more moves to be entered after someone one

--- script.js
+++ script.js
@@ -68,6 +68,8 @@ const GameFlow = (function () {
   const playerOne = Player("Player 1", "O");
   const playerTwo = Player("Player 2", "X"); // creating the players in the game
 
+  let roundOver = false; // status of the game signifying if a round has been won or tied.
+
   let gameDisplay = document.querySelector("#display");
 
   gameDisplay.innerText = "O's turn";
@@ -81,83 +83,88 @@ const GameFlow = (function () {
   gameBoard.gameBoardDisplay.forEach((cell) => {
     cell.addEventListener(
       "click",
-      (e) => {
-        playerArray.forEach((player) => {
-          if (player.isActive === true && e.target.innerText === "") {
-            e.target.innerText = player.marker; // setting the cell to the player with the active status
-            player.isActive = false;
-            e.target.innerText === "X"
-              ? (gameDisplay.innerText = "O's turn")
-              : (gameDisplay.innerText = "X's turn"); // updates display as game goes along
+      function addMarker(e) {
+        if (roundOver === false) {
+          playerArray.forEach((player) => {
+            if (player.isActive === true && e.target.innerText === "") {
+              e.target.innerText = player.marker; // setting the cell to the player with the active status
+              player.isActive = false;
+              e.target.innerText === "X"
+                ? (gameDisplay.innerText = "O's turn")
+                : (gameDisplay.innerText = "X's turn"); // updates display as game goes along
 
-            let gameBoardValues = {
-              topRow: [
-                gameBoard.gameBoardDisplay[0].innerText,
-                gameBoard.gameBoardDisplay[1].innerText,
-                gameBoard.gameBoardDisplay[2].innerText,
-              ],
-              middleRow: [
-                gameBoard.gameBoardDisplay[3].innerText,
-                gameBoard.gameBoardDisplay[4].innerText,
-                gameBoard.gameBoardDisplay[5].innerText,
-              ],
-              bottomRow: [
-                gameBoard.gameBoardDisplay[6].innerText,
-                gameBoard.gameBoardDisplay[7].innerText,
-                gameBoard.gameBoardDisplay[8].innerText,
-              ],
-              leftColumn: [
-                gameBoard.gameBoardDisplay[0].innerText,
-                gameBoard.gameBoardDisplay[3].innerText,
-                gameBoard.gameBoardDisplay[6].innerText,
-              ],
-              middleColumn: [
-                gameBoard.gameBoardDisplay[1].innerText,
-                gameBoard.gameBoardDisplay[4].innerText,
-                gameBoard.gameBoardDisplay[7].innerText,
-              ],
-              rightColumn: [
-                gameBoard.gameBoardDisplay[2].innerText,
-                gameBoard.gameBoardDisplay[5].innerText,
-                gameBoard.gameBoardDisplay[8].innerText,
-              ],
-              leftDiagonal: [
-                gameBoard.gameBoardDisplay[0].innerText,
-                gameBoard.gameBoardDisplay[4].innerText,
-                gameBoard.gameBoardDisplay[8].innerText,
-              ],
-              rightDiagonal: [
-                gameBoard.gameBoardDisplay[2].innerText,
-                gameBoard.gameBoardDisplay[4].innerText,
-                gameBoard.gameBoardDisplay[6].innerText,
-              ],
-            };
+              let gameBoardValues = {
+                topRow: [
+                  gameBoard.gameBoardDisplay[0].innerText,
+                  gameBoard.gameBoardDisplay[1].innerText,
+                  gameBoard.gameBoardDisplay[2].innerText,
+                ],
+                middleRow: [
+                  gameBoard.gameBoardDisplay[3].innerText,
+                  gameBoard.gameBoardDisplay[4].innerText,
+                  gameBoard.gameBoardDisplay[5].innerText,
+                ],
+                bottomRow: [
+                  gameBoard.gameBoardDisplay[6].innerText,
+                  gameBoard.gameBoardDisplay[7].innerText,
+                  gameBoard.gameBoardDisplay[8].innerText,
+                ],
+                leftColumn: [
+                  gameBoard.gameBoardDisplay[0].innerText,
+                  gameBoard.gameBoardDisplay[3].innerText,
+                  gameBoard.gameBoardDisplay[6].innerText,
+                ],
+                middleColumn: [
+                  gameBoard.gameBoardDisplay[1].innerText,
+                  gameBoard.gameBoardDisplay[4].innerText,
+                  gameBoard.gameBoardDisplay[7].innerText,
+                ],
+                rightColumn: [
+                  gameBoard.gameBoardDisplay[2].innerText,
+                  gameBoard.gameBoardDisplay[5].innerText,
+                  gameBoard.gameBoardDisplay[8].innerText,
+                ],
+                leftDiagonal: [
+                  gameBoard.gameBoardDisplay[0].innerText,
+                  gameBoard.gameBoardDisplay[4].innerText,
+                  gameBoard.gameBoardDisplay[8].innerText,
+                ],
+                rightDiagonal: [
+                  gameBoard.gameBoardDisplay[2].innerText,
+                  gameBoard.gameBoardDisplay[4].innerText,
+                  gameBoard.gameBoardDisplay[6].innerText,
+                ],
+              };
 
-            const winningCombinations = [
-              player.marker,
-              player.marker,
-              player.marker,
-            ];
+              const winningCombinations = [
+                player.marker,
+                player.marker,
+                player.marker,
+              ];
 
-            for (let row in gameBoardValues) {
-              if (
-                JSON.stringify(gameBoardValues[row]) ===
-                JSON.stringify(winningCombinations)
-              ) {
-                gameDisplay.innerText = `${player.marker} Won!`;
+              for (let row in gameBoardValues) {
+                if (
+                  JSON.stringify(gameBoardValues[row]) ===
+                  JSON.stringify(winningCombinations)
+                ) {
+                  gameDisplay.innerText = `${player.marker} Won!`;
+                  roundOver = true;
+                }
               }
+
+              const tieCheck = Object.values(gameBoardValues);
+
+              console.log(tieCheck);
+            } else {
+              player.isActive = true;
             }
-
-            const tieCheck = Object.values(gameBoardValues);
-
-            console.log(tieCheck);
-          } else {
-            player.isActive = true;
-          }
-        });
+          });
+        }
       },
       { once: true } // added the "once" option to remove the original event listener so that the event listener wouldn't be triggered if a cell that already has a value was clicked again.
     ); // event listener to add marker to cells;
-  }); // adds markers to the cell
+  });
+
+  //
 })();
 // Module design for controlling how the gameboard display looks and how it will be returned
